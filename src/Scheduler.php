ccb0d364164eb8deed264444c402fd7cef99a5ce
Vadim <enocryph@yandex.ru>
2018-12-05 16:20:03 +0300

    fix Scheduler

--- src/Scheduler.php
+++ src/Scheduler.php
@@ -6,6 +6,10 @@ namespace Scheduler;
 use Scheduler\Interfaces\ApplicationInterface;
 use Scheduler\Interfaces\SchedulerInterface;
 
+/**
+ * Class Scheduler
+ * @package Scheduler
+ */
 class Scheduler implements SchedulerInterface
 {
     /**
@@ -13,6 +17,10 @@ class Scheduler implements SchedulerInterface
      */
     private $scheduledApplications;
 
+    /**
+     * @param ApplicationInterface $application
+     * @param bool $ignoreCached
+     */
     public function scheduleApplication(ApplicationInterface $application, bool $ignoreCached = true): void
     {
         if (!isset($this->scheduledApplications[$application->getApplicationId()])) {
@@ -21,6 +29,9 @@ class Scheduler implements SchedulerInterface
         }
     }
 
+    /**
+     * @param ApplicationInterface $application
+     */
     public function removeScheduledApplication(ApplicationInterface $application): void
     {
         if (!isset($this->scheduledApplications[$application->getApplicationId()])) {
@@ -28,28 +39,39 @@ class Scheduler implements SchedulerInterface
         }
     }
 
+    /**
+     * @return array
+     */
     public function getScheduledApplications(): array
     {
         return $this->scheduledApplications;
     }
 
+    /**
+     * @param \DateTime $time
+     * @return ApplicationInterface[]
+     */
     public function getScheduledApplicationsForTime(\DateTime $time): array
     {
         $timezone = new \DateTimeZone('UTC');
         $today = new \DateTime(gmdate("Y-m-d"), $timezone);
         $minutes = ($time->getTimestamp() - $today->getTimestamp()) / 60;
 
-        $application = [];
+        $applications = [];
 
         foreach ($this->scheduledApplications as $application) {
             if (in_array($minutes, $application->getSchedule())) {
-                $application[] = $application;
+                $applications[] = $application;
             }
         }
 
-        return $application;
+        return $applications;
     }
 
+    /**
+     * @param ApplicationInterface[] $applications
+     * @param bool $ignoreCached
+     */
     public function updateScheduleForApplications($applications, bool $ignoreCached = true): void
     {
         foreach ($applications as $application) {
@@ -59,6 +81,10 @@ class Scheduler implements SchedulerInterface
         }
     }
 
+    /**
+     * @param int $applicationId
+     * @return null|ApplicationInterface
+     */
     public function getApplicationById(int $applicationId): ?ApplicationInterface
     {
         return isset($this->scheduledApplications[$applicationId]) ? $this->scheduledApplications[$applicationId] : null;
